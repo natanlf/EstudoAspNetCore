c4a626994ec645249d51c2372cfa31610775611a
Natan <natanlaraferreira@gmail.com>
2020-05-07 10:22:54 -0300

    Banco de dados e MimicContext
    Instalar biblioteca pelo gerenciador de pacotes NuGet
    Microsoft.Enity.Framework.Core.sqlite
    
    Ferramentas -> Gerenciador de Pacotes do NuGet e digitar o comando e clicar enter:
    Add-Migration BancoInicial
    
    Assim vai gerar um arquivo de migration
    
    Com o comando Update-Database faço o migration rodar no banco de dados.
    Assim o banco Mimic.db será criado
    
    Podemos usar o LINQPad 5 para ver o banco

---
+++ EstudoWebApiAspNetCore/Database/MimicContext.cs
@@ -0,0 +1,21 @@
+﻿using EstudoWebApiAspNetCore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EstudoWebApiAspNetCore.Database
+{
+    public class MimicContext : DbContext
+    {
+
+        public MimicContext(DbContextOptions<MimicContext> options) : base(options)
+        {
+                
+        }
+
+        public DbSet<Palavra> Palavras { get; set; } //tabela
+
+    }
+}
--- EstudoWebApiAspNetCore/Startup.cs
+++ EstudoWebApiAspNetCore/Startup.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EstudoWebApiAspNetCore.Database;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace EstudoWebApiAspNetCore
@@ -15,6 +17,9 @@ namespace EstudoWebApiAspNetCore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MimicContext>(opt => {
+                opt.UseSqlite("Data Source=Database\\Mimic.db");
+            });
             services.AddMvc();
         }
 
