f0a74a9dc9404f69c0dc93a64b7cfb90d8dd40e1
Natan <natanlaraferreira@gmail.com>
2020-05-07 10:56:43 -0300

    Adicionando Controller

---
+++ EstudoWebApiAspNetCore/Controllers/PalavrasController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EstudoWebApiAspNetCore.Controllers
+{
+    [Route("api/palavras")]
+    public class PalavrasController : ControllerBase 
+    {
+        //APP -- /api/palavras
+        [Route("")]
+        [HttpGet]
+        public ActionResult ObterTodas()
+        {
+            return Ok(true);
+        }
+    }
+}
